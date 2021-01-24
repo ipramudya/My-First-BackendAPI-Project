af619b6cebe282bd2d8ccb772dbb0c8b610b8407
unknown <theprimedownload@gmail.com>
2021-01-24 07:53:50 +0700

    Mengimplementasi fungsionalitas CRUD pada controlers

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,43 +1,97 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //#desc         Mengambil seluruh data bootcamps
 //#route        GET /api/v1/bootcamps
 //#access       Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Showing all Bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const getBootcamps = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: getBootcamps,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //#desc         Mengambil salah satu data bootcamp
 //#route        GET /api/v1/bootcamps/:id
 //#access       Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `Showing Single Bootcamp of id ${req.params.id} `,
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const getBootcampID = await Bootcamp.findById(req.params.id);
+    res.status(200).json({
+      success: true,
+      data: getBootcampID,
+    });
+
+    if (!getBootcampID) {
+      return res.status(400).json({ success: false });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //#desc         Membuat data bootcamps baru
 //#route        POST /api/v1/bootcamps
 //#access       Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Creating new Bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const createBootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: createBootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //#desc         Mengupdate data bootcamp
 //#route        PUT /api/v1/bootcamps/:id
 //#access       Private
-exports.updateBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `Update Bootcamp of id ${req.params.id} `,
-  });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const updateBootcamp = await Bootcamp.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.status(200).json({
+      success: true,
+      data: updateBootcamp,
+    });
+
+    if (!updateBootcamp) {
+      res.status(400).json({ success: false });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //#desc         Menghapus data bootcamp tertentu
 //#route        DELETE /api/v1/bootcamps/:id
 //#access       Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: `Delete Bootcamp of id ${req.params.id}`,
-  });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const deleteBootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: `Data of ID ${req.params.id} has been Deleted !`,
+      });
+
+    if (!deleteBootcamp) {
+      return res.status(400).json({ success: false });
+    }
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
